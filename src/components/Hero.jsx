ee8f73aab8e2d9e88c2b6aa0af439d8e809a0f85
rmerzak <rmerzak@student.1337.ma>
2023-12-10 18:13:05 +0100

    fix error mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-          <MarsCanvas />
+          {/* <MarsCanvas /> */}
       <StarsCanvas />
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
