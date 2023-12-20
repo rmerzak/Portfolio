ce803c530bdb0e7b6240fea8a477b03ad791995a
rmerzak <rmerzak@student.1337.ma>
2023-12-20 17:16:26 +0100

    add more backround images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import {  StarsCanvas, MarsCanvas, JubiterCanvas } from './canvas'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { StarsCanvas, MarsCanvas, JubiterCanvas } from './canvas';
+
 const Hero = () => {
+  const [bgIndex, setBgIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBgIndex((prevIndex) => (prevIndex + 1) % 4);
+    }, 3200); // 30 seconds
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const bgImages = ['/bg1.jpeg', '/bg.jpeg', '/bg2.jpeg' , '/bg3.jpeg'];
+
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -11,25 +24,27 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-50 violet-gradient' />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'> Rabi</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915eff]'> Rabi</span>
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          A software architect and enthusiastic developer<br className='sm:block hidden' /> deeply passionate about emerging technologies. 
+            A software architect and enthusiastic developer
+            <br className='sm:block hidden' /> deeply passionate about emerging technologies.
           </p>
         </div>
       </div>
-      <div className=' inset-0 flex justify-center items-center'>
-        <img src='/bg.jpeg' alt='hero' className='h-[100%] w-[100%]' />
-        </div>
-          {/* <MarsCanvas /> */}
-      {/* <StarsCanvas /> */}
+      <div className='inset-0 flex justify-center items-center'>
+        <img src={bgImages[bgIndex]} alt='hero' className='h-[100%] w-[100%]' />
+      </div>
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 '>
             <motion.div className='w-[3px] h-[20px] bg-secondary rounded-full' initial={{ y: 0 }} animate={{ y: 20 }} transition={{ repeat: Infinity, repeatType: 'reverse', duration: 0.6 }} />
-          </div></a>
+          </div>
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
